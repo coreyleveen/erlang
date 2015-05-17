4854f88d6edcde41daf78a9fe17e4c657b85153a
Corey Leveen <mntj11@gmail.com>
2015-05-16 23:02:03 -0400

    Add multiple operator expressions and non base 10 integers

--- numbers.erl
+++ numbers.erl
@@ -1,3 +1,6 @@
+% Expressions must end with a period
+% and be followed by whitespace:
+
 2 + 15.
 
 49 * 100.
@@ -6,6 +9,21 @@
 
 5 / 2.
 
-5 div 2.
+5 div 2. % Int division
 
-5 rem 2.
+5 rem 2. % modulo
+
+(50 * 100) - 4999.
+
+-(50 * 100 - 4999).
+
+-50 * (100 - 4999).
+
+
+% Non base 10 integers:
+
+2#101010. % 1
+
+8#0677. % 447
+
+16#AE. % 174
