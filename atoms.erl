09d1071553b96e8d0088dc91cb0d86c5e84eada8
Corey Leveen <mntj11@gmail.com>
2015-05-16 23:32:50 -0400

    Atoms

---
+++ atoms.erl
@@ -0,0 +1,12 @@
+% Atoms are like immutable literals with their own names as values. They begin
+% with lowercase characters:
+
+atom. % atom
+
+atoms_rule. % atoms_rule
+
+% Though they can be cheated:
+
+'Atoms can be cheated'. % 'Atoms can be cheated'
+
+atom = 'atom'. % atom
